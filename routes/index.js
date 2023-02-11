dad7bfe52e07134840bb34be3cc6daab9eb3c1af
Nitroc1701 <nitrocdev@gmail.com>
2023-02-11 13:54:04 +0100

    style(index) : run code:fix

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 /* GET home page */
-router.get('/', function(req, res, next) {
-  return res.redirect('https://nitroc.xyz');
-});
+router.get('/', function (req, res, next) {
+  return res.redirect('https://nitroc.xyz')
+})
 
 /* POST home */
 router.post('/', (req, res, next) => {
@@ -12,20 +12,21 @@ router.post('/', (req, res, next) => {
   return res.json({
     name: 'Hermes API',
     version: '1.0.0',
-    description: 'Hermes API is a simple API made by Nitroc to fetch basic data across his projects and for you to explore.',
+    description:
+            'Hermes API is a simple API made by Nitroc to fetch basic data across his projects and for you to explore.',
     author: {
       username: 'Nitroc',
       links: {
         website: 'https://nitroc.xyz',
-        github: 'https://github.com/Nitroc1701',
+        github: 'https://github.com/Nitroc1701'
       },
 
       contact: {
         email: 'contact@nitroc.xyz',
-        discord: 'https://discordapp.com/users/396385749664137219',
-      },
-    },
-  });
-});
+        discord: 'https://discordapp.com/users/396385749664137219'
+      }
+    }
+  })
+})
 
-module.exports = router;
+module.exports = router
