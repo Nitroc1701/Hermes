914b0862e95e5e9f7588e073e4d8d4b55dd80243
Nitroc1701 <nitrocdev@gmail.com>
2023-02-11 13:43:36 +0100

    feat(index): POST / : basic info about api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res, next) {
-  return res.redirect('https://nitroc.xyz')
+  return res.redirect('https://nitroc.xyz');
+});
+
+/* POST home */
+router.post('/', (req, res, next) => {
+  // Return basic informations about the api
+  return res.json({
+    name: 'Hermes API',
+    version: '1.0.0',
+    description: 'Hermes API is a simple API made by Nitroc to fetch basic data across his projects and for you to explore.',
+    author: {
+      username: 'Nitroc',
+      links: {
+        website: 'https://nitroc.xyz',
+        github: 'https://github.com/Nitroc1701',
+      },
+
+      contact: {
+        email: 'contact@nitroc.xyz',
+        discord: 'https://discordapp.com/users/396385749664137219',
+      },
+    },
+  });
 });
 
 module.exports = router;
