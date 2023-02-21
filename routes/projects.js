81a96122c7768305007383c0e0812373927270f5
Nitroc1701 <nitrocdev@gmail.com>
2023-02-21 22:35:05 +0100

    feat(app) : POST / to create a project

--- routes/projects.js
+++ routes/projects.js
@@ -6,3 +6,31 @@ const Project = require('../models/Project')
 router.get('/', (req, res, next) => {
     return res.status(200).json(Project.getAll())
 })
+
+router.post('/', (req, res, next) => {
+    const { 
+        name,
+        description,
+        version,
+        author,
+        license,
+        repository,
+    } = req.body;
+
+    if (!name || !description || !version || !author || !license || !repository) {
+        return res.status(404).json({
+            error: 'Missing required fields'
+        })
+    }
+
+    const project = Project.create({
+        name,
+        description,
+        version,
+        author,
+        license,
+        repository,
+    })
+
+    return res.status(201).json(project)
+})
