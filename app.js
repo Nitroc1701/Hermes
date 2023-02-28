eeb37b362cea3c18aa166caa2c32d8ba0e748e35
Nitroc1701 <nitrocdev@gmail.com>
2023-02-28 21:36:43 +0100

    feat(app) : projects routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
+const projectsRouter = require('./routes/projects')
 
 const app = express()
 
@@ -14,5 +15,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
+app.use('/projects', projectsRouter)
 
 module.exports = app
--- routes/projects.js
+++ routes/projects.js
@@ -3,27 +3,31 @@ const router = express.Router()
 
 const Project = require('../models/Project')
 
-router.get('/', (req, res, next) => {
-    return res.status(200).json(Project.getAll())
+router.get('/', async (req, res, next) => {
+    return res.status(200).json(Project.getAllProjects())
 })
 
-router.post('/', (req, res, next) => {
-    const { 
-        name,
-        description,
-        version,
-        author,
-        license,
-        repository,
-    } = req.body;
+router.post('/', async (req, res, next) => {
+    const { name, description, version, author, license, repository } = req.body
 
-    if (!name || !description || !version || !author || !license || !repository) {
+    const id = await getNextId();
+
+    if (
+        !id ||
+        !name ||
+        !description ||
+        !version ||
+        !author ||
+        !license ||
+        !repository
+    ) {
         return res.status(404).json({
-            error: 'Missing required fields'
+            error: 'Missing required fields',
         })
     }
 
-    const project = Project.create({
+    const project = await Project.createProject({
+        id,
         name,
         description,
         version,
@@ -32,60 +36,53 @@ router.post('/', (req, res, next) => {
         repository,
     })
 
-    return res.status(201).json(project)
+    return res.status(200).json(project)
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
     const { id } = req.params
 
-    const project = Project.findById(id)
+    const project = await Project.findProjectById(parseInt(id, 10))
 
     if (!project) {
         return res.status(404).json({
-            error: 'Project not found'
+            error: 'Project not found',
         })
     }
 
     return res.status(200).json(project)
 })
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
     const { id } = req.params
 
-    const project = Project.findById(id)
+    const project = await Project.findProjectById(parseInt(id, 10))
 
     if (!project) {
         return res.status(404).json({
-            error: 'Project not found'
+            error: 'Project not found',
         })
     }
 
-    Project.delete(id)
+    await Project.deleteProject(id)
 
-    return res.status(204).json()
+    return res.sendStatus(200)
 })
 
-router.patch('/:id', (req, res, next) => {
+router.patch('/:id', async (req, res, next) => {
     const { id } = req.params
 
-    const project = Project.findById(id)
+    const project = await Project.findProjectById(parseInt(id, 10))
 
     if (!project) {
         return res.status(404).json({
-            error: 'Project not found'
+            error: 'Project not found',
         })
     }
 
-    const {
-        name,
-        description,
-        version,
-        author,
-        license,
-        repository,
-    } = req.body
+    const { name, description, version, author, license, repository } = req.body
 
-    const updatedProject = Project.update(id, {
+    const updatedProject = await Project.updateProject(parseInt(id, 10), {
         name,
         description,
         version,
@@ -96,5 +93,14 @@ router.patch('/:id', (req, res, next) => {
 
     return res.status(200).json(updatedProject)
 })
+
+const getNextId = async () => {
+    const projects = await Project.getAllProjects();
+    const lastItemIndex = projects?.length !== 0 ? projects.length - 1 : undefined;
+    if (lastItemIndex === undefined) return 1;
+    const lastId = projects[lastItemIndex]?.id;
+    const nextId = lastId + 1;
+    return nextId;
+}
 
 module.exports = router
